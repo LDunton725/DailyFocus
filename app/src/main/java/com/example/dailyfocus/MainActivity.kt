49d40b0642dc9f842dc9e2355fb6708e4261b9df
LDunton725 <ldunton@smartweb.augustatech.edu>
2026-03-09 14:43:27 -0400

    3/9/2026

--- app/src/main/java/com/example/dailyfocus/MainActivity.kt
+++ app/src/main/java/com/example/dailyfocus/MainActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
+// Updated today
 class MainActivity : AppCompatActivity() {
 
     private lateinit var tvTask: TextView
@@ -16,10 +19,30 @@ class MainActivity : AppCompatActivity() {
 
         tvTask = findViewById(R.id.tvTask)
         val btnAddTask = findViewById<Button>(R.id.btnAddTask)
+        val btnSaveTask = findViewById<Button>(R.id.btnSaveTask)
+        val btnLoadTask = findViewById<Button>(R.id.btnLoadTask)
 
         btnAddTask.setOnClickListener {
             val intent = Intent(this, AddTaskActivity::class.java)
             startActivity(intent)
+        }
+
+        btnSaveTask.setOnClickListener {
+            val text = tvTask.text.toString()
+            val fileOutput: FileOutputStream = openFileOutput("task_file.txt", MODE_PRIVATE)
+            fileOutput.write(text.toByteArray())
+            fileOutput.close()
+        }
+
+        btnLoadTask.setOnClickListener {
+            try {
+                val fileInput: FileInputStream = openFileInput("task_file.txt")
+                val text = fileInput.bufferedReader().readText()
+                tvTask.text = text
+                fileInput.close()
+            } catch (_: Exception) {
+                tvTask.text = getString(R.string.no_saved_task_found)
+            }
         }
     }
 
